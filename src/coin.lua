18cb129b694221650c741e206471c87c420c4e1c
John Kozlovskyi <jko@siteimprove.com>
2026-03-06 13:29:08 +0000

    dude: task 2: Create shadow rendering system in coin.lua that draws dark elliptical shadows beneath coins

--- src/coin.lua
+++ src/coin.lua
@@ -10,7 +10,24 @@ function Coin:new(x, y, radius, color)
     coin.radius = radius or 10
     coin.color = color or {1, 1, 0}  -- yellow by default
     
+    -- Shadow properties
+    coin.shadowOffset = {x = 2, y = 3}  -- Smaller shadow offset for coins
+    coin.shadowColor = {0, 0, 0, 0.25}  -- Semi-transparent black shadow, slightly lighter than balls
+    coin.shadowScale = {x = 1.1, y = 0.5}  -- Shadow is wider and flatter than coin
+    
     return coin
+end
+
+function Coin:drawShadow()
+    -- Draw shadow as an ellipse beneath the coin
+    love.graphics.setColor(self.shadowColor[1], self.shadowColor[2], self.shadowColor[3], self.shadowColor[4])
+    
+    local shadowX = self.x + self.shadowOffset.x
+    local shadowY = self.y + self.shadowOffset.y
+    local shadowRadiusX = self.radius * self.shadowScale.x
+    local shadowRadiusY = self.radius * self.shadowScale.y
+    
+    love.graphics.ellipse("fill", shadowX, shadowY, shadowRadiusX, shadowRadiusY)
 end
 
 function Coin:draw()
